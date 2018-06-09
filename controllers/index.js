0d06a2f5c5042393ede37023eae647c13499006f
Allan Assis <allan-bdc@hotmail.com>
2018-06-09 19:33:18 -0300

    Retirando console.log

--- controllers/index.js
+++ controllers/index.js
@@ -3,8 +3,6 @@ let indexController = function(expressao){
 
     let array = expressao.split('_');
     
-    console.log(array)
-
     while(array.length != 1){
 
         if(array.indexOf('/') > 0){
@@ -13,9 +11,7 @@ let indexController = function(expressao){
             let result = Number.parseFloat(array[index - 1]) / Number.parseFloat(array[index + 1]);
 
             array.splice(index - 1, 3, result.toString());
-
-            console.log(array);
-            
+                      
         }
         else if(array.indexOf('*') > 0){
 
@@ -24,7 +20,6 @@ let indexController = function(expressao){
 
             array.splice(index - 1, 3, result.toString());
             
-            console.log(array);
         }
         else if(array.indexOf('+') > 0){
 
@@ -32,8 +27,7 @@ let indexController = function(expressao){
             let result = Number.parseFloat(array[index - 1]) + Number.parseFloat(array[index + 1]);
 
             array.splice(index - 1, 3, result.toString());
-
-            console.log(array);
+            
         }
         else if(array.indexOf('-') > 0){
 
@@ -42,11 +36,9 @@ let indexController = function(expressao){
 
             array.splice(index - 1, 3, result.toString());
 
-            console.log(array);
         }
     }
     return (array[0]);
-    console.log(array);
 }
 
 module.exports = indexController;
